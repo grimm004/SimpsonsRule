25506f022bf152a79a8d3c937dcc4736bd766c2d
grimm004 <grimm004@gmail.com>
2017-08-27 02:17:59 +0100

    Changed Types and output Formatting 2

--- SimpsonsIntegralEstimator/SimpsonsIntegralEstimator.cpp
+++ SimpsonsIntegralEstimator/SimpsonsIntegralEstimator.cpp
@@ -3,12 +3,12 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <cmath>
+#include <stdio.h>
+#include <math.h>
 
 static double f(double x)
 {
 	return 10 / ((3 * (x * x)) - (2 * x) + log(x));
-	//return pow();
 }
 
 static double t(long n, double start, double end)
@@ -16,7 +16,7 @@ static double t(long n, double start, double end)
 	double interval = (end - start) / n;
 	double total = 0;
 	double current = start;
-	for (int i = 0; i < n - 1; i++) total += f((current += interval));
+	for (long i = 0; i < n - 1; i++) total += f((current += interval));
 	return (interval * (f(start) + f(end) + (2 * total))) / 2;
 }
 
@@ -25,7 +25,7 @@ static double m(long n, double start, double end)
 	double interval = (end - start) / n;
 	double total = 0;
 	double current = start;
-	for (int i = 0; i < n; i++) total += f((current + (current += interval)) / 2);
+	for (long i = 0; i < n; i++) total += f((current + (current += interval)) / 2);
 	return interval * total;
 }
 
@@ -34,34 +34,20 @@ static double s(double tn, double mn)
 	return (tn + (2 * mn)) / 3;
 }
 
-void printl(char* text)
-{
-	std::cout << text << std::endl;
-}
-
-void printl(double number)
-{
-	std::cout << number << std::endl;
-}
-
-void printl()
-{
-	std::cout << std::endl;
-}
-
 int main()
 {
-	long n = 1073741824;
-	int start = 2;
-	int end = 4;
+	for (short i = 30; i < 40; i++)
+	{
+		long n = (long)pow(2, i);
+		int start = 2;
+		int end = 4;
 
-	double tn = t(n, start, end);
-	double mn = m(n, start, end);
-	double sn = s(tn, mn);
+		double tn = t(n, start, end);
+		double mn = m(n, start, end);
+		double sn = s(tn, mn);
 
-	printl(tn);
-	printl(mn);
-	printl(sn);
+		printf("i: %u\tn: %u\tTn: %.20f\tMn: %.20f\tSn: %.20f\n", i, n, tn, mn, sn);
+	}
 
 	std::cin.get();
 
